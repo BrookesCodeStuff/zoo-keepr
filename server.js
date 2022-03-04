bd25107ee1827127b535238749c8084b3d4d4105
Brooke <brookepaglia@gmail.com>
2022-03-04 15:15:22 -0500

    add POST and middleware

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const { animals } = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// Parse incoming JSON data
+app.use(express.json());
+
 function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
@@ -23,6 +29,12 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
